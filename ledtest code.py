e18565c2fa97ea5e3d843501965303046c18ada7
cfdj <callumderry@gmail.com>
2017-05-03 11:51:19 +0100

    added setmode

--- ledtest code.py
+++ ledtest code.py
@@ -10,6 +10,8 @@
 #-------------------------------------------------------------------------------
 import RPi.GPIO as GPIO
 import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 GPIO.setup(5,GPIO.OUT)
 GPIO.setup(6,GPIO.OUT)
