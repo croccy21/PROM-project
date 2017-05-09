050291467597f434ed4f3b82b15253b24b3040c1
AristiCthonia <rachelalisonc@gmail.com>
2017-05-09 14:12:20 +0100

    now functional (just needs lower voltage drop

--- sideattackcode.py
+++ sideattackcode.py
@@ -19,8 +19,8 @@
 import time
 import smbus
 
-BUS_ADDRESS = 0x38
-LOCK_PICK_ADDRESS = 0b01110000
+BUS_ADDRESS = 1
+LOCK_PICK_ADDRESS = 0b0111000
 INPUT_MASK = 0x8f
 
 DIGITS = ["1","2","3","4","5","6","7","8","9","*","0","#"]
@@ -32,40 +32,46 @@ DIGIT_MAP = {"1":(0,0),"2":(0,1),"3":(0,2),
 
 def main():
     bus = smbus.SMBus(BUS_ADDRESS)
+    find_code(bus)
 
 def find_code(bus):
     code = []
 
     for i in range(4):
         found = False
-        digit_indix = 0
-        while not found and digit_indix<len(DIGITS):
+        digit_index = 0
+        while not found and digit_index<len(DIGITS):
             #send code so far
+            print('sending existing code:' + str(code))
             for digit in code:
                 send_digit(digit, bus)
 
             #test next digit
-            digit = DIGITS[digit_indix]
+            digit = DIGITS[digit_index]
+            print('testing digit' + str(digit))
             send_digit(digit, bus)
 
             resp = get_line(bus, 7)
 
             if resp:
+                print('got it')
                 code.append(digit)
                 found = True
             else:
+                print('try agian')
                 digit_index += 1
     return code
 
 def get_line(bus, bit):
-    bus.write_byte(INPUT_MASK)
+    bus.write_byte(LOCK_PICK_ADDRESS, INPUT_MASK)
+    time.sleep(0.1)
     inp = bus.read_byte(LOCK_PICK_ADDRESS)
+    time.sleep(0.1)
 
     #return the value of the bit at position bit
     return inp & (1 << bit)
 
 def send_digit(digit, bus):
-
     row_bit, column_bit = DIGIT_MAP[digit]
     #wait for line to go low
     while get_line(bus, row_bit) == 1:
@@ -73,4 +79,8 @@ def send_digit(digit, bus):
 
     #shift by 4 and invert (as active low)
     column_code = ~(column_bit<<4)
-    bus.write(LOCK_PICK_ADDRESS, column_code)
+    bus.write_byte(LOCK_PICK_ADDRESS, column_code)
+    time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
