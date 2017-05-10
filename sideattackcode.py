9a18c7e8cc80a5d5857935bfcf559dd4e2520af7
AristiCthonia <rachelalisonc@gmail.com>
2017-05-10 16:59:30 +0100

    Now occationally sends something recognisable

--- sideattackcode.py
+++ sideattackcode.py
@@ -32,9 +32,11 @@ DIGIT_MAP = {"1":(0,0),"2":(0,1),"3":(0,2),
              "7":(2,0),"8":(2,1),"9":(2,2),
              "*":(3,0),"0":(3,1),"#":(3,2)}
 
+
 def debug(level, arg):
     if level<=DEBUG_LEVEL:
-        print('/t'*level+str(level) + ") " + str(arg))
+        print('\t' * level + str(level) + ") " + str(arg))
+
 
 def main():
     bus = smbus.SMBus(BUS_ADDRESS)
@@ -55,12 +57,11 @@ def find_code(bus):
 
             #test next digit
             digit = DIGITS[digit_index]
-            debug(1, 'testing digit: ' + str(digit))
-            send_digit(digit, bus)
+            debug(1, 'testing digit '+ str(digit))
 
-            resp = get_next_line(bus)
+            resp = send_digit(digit, bus)
 
-            if resp==7:
+            if resp!=7:
                 debug(1, 'got it')
                 code.append(digit)
                 found = True
@@ -69,38 +70,50 @@ def find_code(bus):
                 digit_index += 1
     return code
 
+
 def write(bus, byte, mask=0xff, current_byte = 0x00):
     byte = (byte & mask) | (current_byte & ~mask)
     bus.write_byte(LOCK_PICK_ADDRESS, byte)
     return byte
 
-def read(bus, mask=0xff):
+
+def read(bus, mask=0xff, active_low=True):
     byte = bus.read_byte(LOCK_PICK_ADDRESS)
-    debug(3, bin(byte))
-    return byte & mask
+    debug(4, bin(byte))
+    if active_low:
+        byte = mask & ~(byte & mask)
+    else:
+        byte = mask & byte
+    debug(4, bin(byte))
+    return byte
+
 
 def get_next_line(bus, current_byte = 0x00, active_low=True):
-    # Set the lines to read high
+    # Set the lines to read high00000001
     write(bus, INPUT_MASK, INPUT_MASK, current_byte)
-    time.sleep(0.001)
-    byte_read = 0xff if active_low else 0x00
-    debug(2, 'waiting for line')
-    #I'm sorry
-    while not bool(byte_read ^ (0xff if active_low else 0x00)):
-        byte_read = read(bus, INPUT_MASK)
-        time.sleep(0.001)
-    debug(2, 'byte ' + bin(byte_read))
+    #time.sleep(0.0001)
+    byte_read = 0x00
+    debug(3, 'waiting for line')
+
+    while not bool(byte_read):
+        byte_read = read(bus, INPUT_MASK, active_low)
+        #time.sleep(0.0001)
+    debug(3, 'byte {:0>8b}'.format(byte_read))
+
     for bit in range(7, -1, -1):
         bit_mask = 1 << bit
         if bool(INPUT_MASK & bit_mask):
-            if bool(byte_read & bit_mask) != active_low:
+            if bool(byte_read & bit_mask):
                 debug(2, 'found ' + str(bit))
                 return bit
+
     raise ValueError('no valid bit found after read')
+
 
 def wait_for_line(bus, bit, current_byte = 0x00, active_low=True):
     while get_next_line(bus, current_byte, active_low) != bit:
-        time.sleep(0.001)
+        time.sleep(0.0001)
+
 
 def send_digit(digit, bus):
     row_bit, column_bit = DIGIT_MAP[digit]
@@ -109,9 +122,38 @@ def send_digit(digit, bus):
     wait_for_line(bus, row_bit)
 
     #shift by 4 and invert (as active low)
-    column_code = 0xff & ~(column_bit<<4)
-    debug(2, 'sending ' + bin(column_code))
+    column_code = INPUT_MASK | (1<<(column_bit + 4))
+    debug(2, 'sending {:0>8b}'.format(column_code))
     write(bus, column_code)
 
+    time.sleep(.2)
+    debug(2, 'wait for response')
+    return get_next_line(bus)
+
+def test1():
+    bus = smbus.SMBus(BUS_ADDRESS)
+    while 1:
+        print(0)
+        write(bus, 0b10011111)
+        time.sleep(1)
+        write(bus, 0b10001111)
+        time.sleep(1)
+        print(1)
+        write(bus, 0b10101111)
+        time.sleep(1)
+        write(bus, 0b10001111)
+        time.sleep(1)
+        print(2)
+        write(bus, 0b11001111)
+        time.sleep(1)
+        write(bus, 0b10001111)
+        time.sleep(1)
+
+def test2():
+    bus = smbus.SMBus(BUS_ADDRESS)
+    for i in range(20):
+        send_digit('2', bus)
+        time.sleep(1)
+
 if __name__ == "__main__":
-    main()
+    test2()
