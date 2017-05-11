4d6dc8111af39a49633b92bda0004bc028adffff
Joel Goddard <croccy21@users.noreply.github.com>
2017-05-11 16:42:28 +0100

    Final software version working with all extras

--- lock code.py
+++ lock code.py
@@ -15,9 +15,13 @@ tryanddelay.append(60)
 #Time lockout (was told it had to take the start and end tiems and assume these were from boot up)
 #Passed in as an array of hours, and then time past hour in minutes
 #Will return ValueError if minutes >59 or hours > 24
+timestart = []
+timeend = []
 
-timestart[9,59]
-timeend[17,00]
+timestart.append(0)
+timestart.append(0)
+timeend.append(0)
+timeend.append(0)
 if (timestart[1] or timeend[1]) > 59:
     ValueError("Please enter an amount of minutes less than 60")
 elif (timestart[0] or timeend[0]) > 24:
@@ -36,7 +40,7 @@ GPIO.setwarnings(False)
 #The length of sleeps
 length = 500
 #Settting up the load and output-enable (only needs to be done once because always output
-GPIO.setup(14,GPIO.OUT) #load
+GPIO.setup(14, GPIO.OUT) #load
 GPIO.setup(15,GPIO.OUT) #enable
 
 GPIO.output(14,0)
@@ -190,6 +194,7 @@ def main(length, seconds, side_attack, tryanddelay):
     password = read_password()
     open_csv()
     wrong = False;
+    key_pressed_time =time.time()*10
     gap = 0 # used for software debouncing
     position = 0 #the point in the password the person is at
     #the main loop the repeatedly polls each line
@@ -197,6 +202,7 @@ def main(length, seconds, side_attack, tryanddelay):
     timeout = 0 # this counts up continously lighting up the onboard leds until all are lit then resets position
     while wrong ==False:
         if (time.time() > seconds) and (seconds != 0):
+            onboardgpio = [5,6,12,13,16,19,20,26]
             GPIO.output(onboardgpio[0],1)
             GPIO.output(onboardgpio[1],1)
             GPIO.output(onboardgpio[2],1)
@@ -212,7 +218,7 @@ def main(length, seconds, side_attack, tryanddelay):
         ten = 0
         eleven = 0
 
-        #this loop polls each of the rows in order
+        #this loop polls each of key_pressed_timethe rows in order
 
         for index in range (4):
             pressed = poll_row(nine,ten,eleven,length)
@@ -231,17 +237,18 @@ def main(length, seconds, side_attack, tryanddelay):
                 incorrect = 0
                 buttonpressed()
                 position +=1
-                print("{0}: {1} --> {2}".format(index, str(nine) + str(ten) +str(eleven), key_board[index][pressed] if pressed != 100 else "N"))
+                #print("{0}: {1} --> {2}".format(index, str(nine) + str(ten) +str(eleven), key_board[index][pressed] if pressed != 100 else "N"))
                 #This is the code for the digit display, currently looks wrong because of debugging print
                 os.system('clear')
+                item = ""
                 for  i in range (position-1):
-                    print("*", end="")
-                print(key_board[index][pressed])
+                    item = item + "*"
+                print(item + str(key_board[index][pressed]))
                 key_pressed_time = time.time()+1
                 if (position >3):
                     #This is the code for the digit display
                     os.system('clear')
-                    print("****",end = "")
+                    print("****")
                     #runs if the whole password has been entered
                     write_csv("Accepted")
                     ledset("green",True)
@@ -254,9 +261,10 @@ def main(length, seconds, side_attack, tryanddelay):
             else:
                 #This is the code for the digit display:
                 os.system('clear')
-                for  i in range (position):
-                    print("*", end="")
-                print(key_board[index][pressed])
+                item = ""
+                for  i in range (position-1):
+                    item + item+ "*"
+                print(item + str(key_board[index][pressed]))
                 key_pressed_time = time.time()+1
                 #Enables or disables side attacks based on the side_attack constant
                 if side_attack == True:
@@ -288,11 +296,13 @@ def main(length, seconds, side_attack, tryanddelay):
                 if (tryanddelay[0]!= 0):
                     numtrys += 1
                     if (numtrys >= tryanddelay[0]):
-                        numsecondslef = tryanddelay[1]
+                        numsecondsleft = tryanddelay[1]
                         for index in range(tryanddelay[1]):
                             os.system('clear')
-                            print("LOCKED " + str(numseconds))
+                            print("LOCKED " + str(numsecondsleft))
+                            numsecondsleft -=1
                             sleepytime(1000)
+                        numtrys = 0
 
             if (index == 0):
                 eleven = 1
@@ -304,24 +314,25 @@ def main(length, seconds, side_attack, tryanddelay):
                 eleven = 1
                 ten = 1
             #time.sleep(1)
-            print(str(nine) + str(ten) + str(eleven) + (key_board[index][pressed] if pressed != 100 else "N"))
-            timeout += 10
+            #print(str(nine) + str(ten) + str(eleven) + (key_board[index][pressed] if pressed != 100 else "N"))
+            timeout += 5
             values =timer(timeout,position)
             position = values[0]
             timeout = values[1]
             if key_pressed_time <= time.time():
                 os.system('clear')
+                item = ""
                 for  i in range (position):
-                    print("*", end="")
+                    item = item +"*"
+                print(item)
 
 
 def sleepytime(length): #assumes length is an int in ms
     wake = time.time()+0.0005+length/1000
     while wake > time.time():
         time.sleep(length/10000)
-''' Optional stuff
 
-'''
+
 
 
 def open_csv():
@@ -354,9 +365,9 @@ def write_csv(text):
     csv.write(time.asctime(time.localtime())+", "+text+ "\n")
     csv.close()
 
-    if "ac" in text:
+    if "Ac" in text:
         num = 2
-    elif "de" in text:
+    elif "De" in text:
         num = 1
     else:
         num = 0
@@ -386,6 +397,7 @@ else:
         os.system('gnuplot "graph.p"')
         GPIO.cleanup()
     else:
+        onboardgpio = [5,6,12,13,16,19,20,26]
         GPIO.output(onboardgpio[0],1)
         GPIO.output(onboardgpio[1],1)
         GPIO.output(onboardgpio[2],1)
@@ -396,3 +408,6 @@ else:
         GPIO.output(onboardgpio[7],1)
         ledset("red",True)
         sleepytime(10000)
+        GPIO.cleanup()
+
+
